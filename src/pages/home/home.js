b7f4fa8c3b08f91ff3e3ec4123714aa328891566
MayDragon <may-dragon2000@outlook.com>
2023-09-14 03:01:32 +0800

    update map spread text

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -586,22 +586,14 @@ function Home() {
                   </div>
                   <div className="text_box">
                     <p>
-                      Sed ut perspiciatis unde omnis iste natus error sit
-                      voluptatem accusantium doloremque laudantium, totam rem
-                      aperiam, eaque ipsa quae ab illo inventore veritatis et
-                      quasi architecto beatae vitae dicta sunt explicabo. Nemo
-                      enim ipsam voluptatem quia voluptas sit aspernatur aut
-                      odit aut fugit, sed quia consequuntur magni dolores eos
-                      qui ratione voluptatem sequi nesciunt. Neque porro
-                      quisquam est, qui dolorem ipsum quia dolor sit amet,
-                      consectetur, adipisci velit, sed quia non numquam eius
-                      modi tempora incidunt ut labore et dolore magnam aliquam
-                      quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-                      exercitationem ullam corporis suscipit laboriosam, nisi ut
-                      aliquid ex ea commodi consequatur? Quis autem vel eum iure
-                      reprehenderit qui in ea voluptate velit esse quam nihil
-                      molestiae consequatur, vel illum qui dolorem eum fugiat
-                      quo voluptas nulla pariatur?
+                      In the early days, communism found its origins in Russia,
+                      paving the way for subsequent unsuccessful trials in
+                      China. It is from these very foundations that we embarked
+                      upon the dissemination of our unwavering communist
+                      ideology. Grand conquests will be accompanied by burns and
+                      or strategic acquisitions of $COMMUNSIM by our team,
+                      serving as triumphant manifestations of our victory over
+                      enemy regimes.
                     </p>
                   </div>
                 </CardBody>
